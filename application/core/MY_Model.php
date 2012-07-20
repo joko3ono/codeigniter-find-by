2a1d99f892c7591112402c03410e85abe73ae427
joko3ono <joko.triono14@gmail.com>
2012-07-20 15:14:07 +0700

    revise error when using method save & create more dinamyc find_by, find_all_by method

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -1,12 +1,9 @@
-<?php
-
-if (!defined('BASEPATH')){
-    exit('No direct script access allowed');
-}
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class MY_Model extends CI_Model {
     // define attribute table_name
     public $table_name = "";
+    public $id = "";
 
     /*
     *
@@ -29,7 +26,8 @@ class MY_Model extends CI_Model {
     public function __call($method, $params) {
         if (substr($method, 0, 8) == 'find_by_') {
             $length = strlen($method);
-            $query = $this->db->query('SELECT * FROM ' . $this->table_name . ' WHERE ' . $this->table_name . '.' . substr($method, 8, $length + 1) . ' = "' . $params[0] . '" limit 0,1');
+            $str_query = $this->build_query($method, 8, $length, "SELECT * FROM " . $this->table_name, $params);
+            $query = $this->db->query($str_query . ' limit 0,1');
             return $query->row();
         } elseif (substr($method, 0, 8) == 'find_all') {
             $length = strlen($method);
@@ -37,8 +35,12 @@ class MY_Model extends CI_Model {
             return $query->result();
         } elseif (substr($method, 0, 12) == 'find_all_by_'){
             $length = strlen($method);
-            $query = $this->db->query('SELECT * FROM ' . $this->table_name . ' WHERE ' . $this->table_name . '.' . substr($method, 12, $length + 1) . ' = "' . $params[0] . '"');
+            $str_query = $this->build_query($method, 12, $length, "SELECT * FROM " . $this->table_name, $params);
+            $query = $this->db->query($str_query);
             return $query->result();
+        } elseif (substr($method, 0, 13) == 'test_find_by_'){
+            $length = strlen($method) + 1;
+            echo $this->build_query($method, 13, $length, "SELECT * FROM " . $this->table_name, $params);
         } else {
             return null;
         }
@@ -51,7 +53,9 @@ class MY_Model extends CI_Model {
     *
     */
     public function save($data){
-        return $this->db->insert($this->tablename, $data);
+        $status = $this->db->insert($this->table_name, $data);
+        $this->id = $this->db->insert_id();
+        return $status;
     }
 
     /*
@@ -77,6 +81,47 @@ class MY_Model extends CI_Model {
     */
     public function delete($param, $delete_by = 'id'){
         $this->db->where($delete_by,$param);
-        return $this->db->update($this->table_name);
+        return $this->db->delete($this->table_name);
     }
+
+    /*
+    * Private method to generate query
+    * @string string of method name
+    * @start int string that will be invetigate to build query
+    * @end int offset
+    * @query string
+    * @params array() 
+    */ 
+    private function build_query($string, $start, $end, $query, $params){
+        $new_str = substr($string, $start, $end);
+        $arr_str = $this->find_and($new_str);
+        $before = 0;
+        for($i = 0 ; $i < sizeof($arr_str) ; $i ++){
+            if($i == 0){
+                $field = substr($string, $start, $arr_str[$i]);
+                $query = $query . " WHERE " . $this->table_name . "." . $field . " = '" . $params[$i] . "'";
+            }
+            $next = ($i + 1 < sizeof($arr_str) ? $arr_str[$i + 1] : strlen($string));
+            $field = substr($new_str, $arr_str[$i] + 5 + $before, $next);
+            $before = $arr_str[$i] + 5;
+            $query = $query . " AND "  . $this->table_name . "." . $field . " = '" . $params[$i+1] . "'";
+        }
+        return $query;
+    }
+
+    /*
+    * Private method to find _and_ in string
+    * @string String of method
+    * @arr array()
+    */
+    private function find_and($string, $arr = array()){
+        $pos = strpos($string, '_and_');
+        if($pos != false){
+            array_push($arr, $pos);
+            $arr = $this->find_and(substr($string, $pos + 5, strlen($string) + 1), $arr);
+        }
+        //TODO create find 'or' in method name
+        return $arr;
+    }
+
 }
